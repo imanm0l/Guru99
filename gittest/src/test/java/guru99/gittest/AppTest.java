561b3dc3ffc2f924c55f0a3900c85aa95edf332d
imanm0l <43917444+imanm0l@users.noreply.github.com>
2018-10-07 03:23:45 +0530

    Update AppTest.java

--- gittest/src/test/java/guru99/gittest/AppTest.java
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -2,6 +2,7 @@ package guru99.gittest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class AppTest {
